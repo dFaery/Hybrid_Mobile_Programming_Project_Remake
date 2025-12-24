c8a9b718fa74c88f1a242f68a80f0be8e831d5aa
clath1a <alethiaclarita1@gmail.com>
2025-12-24 17:38:57 +0700

    UI bikin berita & daftar berita
     (tp blm sambung ke db)

--- src/app/semua-berita/semua-berita.page.ts
+++ src/app/semua-berita/semua-berita.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  BeritaserviceService,
+  Berita,
+} from '../services/beritaservice.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-semua-berita',
@@ -7,9 +12,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SemuaBeritaPage implements OnInit {
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  beritaSaya: any[] = [];
+  daftarKategori: string[] = ['trending', 'economics', 'technology', 'law'];
+
+  formBerita = {
+    judul: '',
+    deskripsi: '',
+    foto: '',
+    kategori: [],
+  };
+
+  constructor(
+    private beritaservice: BeritaserviceService,
+    private toastCtrl: AlertController
+  ) {}
+
+  // Pengecekan Judul Duplikat
+  async simpanBerita(modal: any) {
+    // 1. Cek apakah judul sudah ada di list (Poin 5)
+    const isExist = this.beritaSaya.some(
+      (b) => b.judul.toLowerCase() === this.formBerita.judul.toLowerCase()
+    );
+
+    if (isExist) {
+      const alert = await this.toastCtrl.create({
+        header: 'Error',
+        message: 'Judul berita sudah pernah dibuat!',
+        buttons: ['OK'],
+      });
+      await alert.present();
+      return;
+    }
+
+    // 2. Jika aman, kirim ke Web Service
+    // logic kirim http post ke PHP...
+    console.log('Data siap kirim:', this.formBerita);
+
+    // 3. Close modal & reset form
+    modal.dismiss();
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.formBerita = { judul: '', deskripsi: '', foto: '', kategori: [] };
+  }
+
+  hapusBerita(id: number) {
+    console.log('Menghapus berita id:', id);
+    // Logika hapus nanti di sini
   }
 
 }
