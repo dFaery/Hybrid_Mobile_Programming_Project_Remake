6b46c98c90a40798ce324fcda5d05b629f372ecf
clath1a <alethiaclarita1@gmail.com>
2025-12-24 17:14:54 +0700

    UI tab kategori, tp blm muncul

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,29 +1,32 @@
-import { BeritaserviceService, Berita } from '../services/beritaservice.service';
+import {
+  BeritaserviceService,
+  Berita,
+} from '../services/beritaservice.service';
 import { Component, inject } from '@angular/core';
-
-
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone:false,
+  standalone: false,
 })
 export class HomePage {
-
   constructor(
+    private route: ActivatedRoute,
     public beritaservice: BeritaserviceService //jangan ditanya kenapa begitu, tp emg begini dr ionicnya :v
   ) {}
   berita: any;
-  jenisTampilan: string = 'trending';
+  jenisTampilan: any;
   beritaDicari: string = '';
   semuaBerita: any[] = [];
   hasilPencarian: any[] = [];
+  categories: any[] = [];
 
+  // untuk fitur search
   cariBeritaByJudul() {
     const lowerKeyword = this.beritaDicari.toLowerCase();
-    this.jenisTampilan = 'search';
+    this.jenisTampilan = 0;
     if (!lowerKeyword) {
       // kalau kosong, dia munculin semua berita
       this.hasilPencarian = [...this.semuaBerita];
@@ -34,4 +37,37 @@ export class HomePage {
     }
   }
 
+  chunkArray(arr: any[], chunkSize: number): any[][] {
+    const result = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      result.push(arr.slice(i, i + chunkSize));
+    }
+    return result;
+  }
+
+  isToastOpen = false;
+  toastMessage = '';
+
+  // Fungsi ini dijalankan sekali saat aplikasi dibuka (ngOnInit)
+loadCategories() {
+  this.beritaservice.getAllKategory().subscribe((response) => {
+    if (response.result === 'OK') {
+      this.categories = response.data; // Ini mengisi tab kategori
+    }
+  });
+}
+
+// Fungsi ini dijalankan saat user mengklik salah satu tab
+pilihKategori(id: any) {
+  this.jenisTampilan = id;
+  this.beritaservice.getBeritaByKategori(id).subscribe((response) => {
+    if (response.result === 'OK') {
+      this.semuaBerita = response.data;
+    }
+  });
+}
+
+  ngOnInit() {
+  this.loadCategories();
+}
 }
--- src/app/services/beritaservice.service.ts
+++ src/app/services/beritaservice.service.ts
@@ -3,69 +3,93 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface Berita {
-  id:number,
-  judul:string,
-  tanggalRilis:string,
-  foto:string,
-  fotoTambahan:string[],
-  isiBerita:string,
-  penerbit:string,
-  rating:number,
-  jumlahReview:number,
-  rekomendasi:number,
-  views:number,
-  kategori:string[]
+  id: number;
+  judul: string;
+  tanggalRilis: string;
+  foto: string;
+  fotoTambahan: string[];
+  isiBerita: string;
+  penerbit: string;
+  rating: number;
+  jumlahReview: number;
+  rekomendasi: number;
+  views: number;
+  kategori: string[];
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BeritaserviceService {
-  url = "https://ubaya.cloud/hybrid/160423183/project.php";
+  url = 'https://ubaya.cloud/hybrid/160423183/project.php';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getAllBerita():Observable<any>{
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+  getAllBerita(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
     const body = new URLSearchParams();
-    body.set('action', "getAllBerita"); 
+    body.set('action', 'getAllBerita');
     const urlEncodedData = body.toString();
 
     return this.http.post(this.url, urlEncodedData, { headers });
   }
 
-  getFavoritBerita(idUser:number):Observable<any>{
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+  getFavoritBerita(idUser: number): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
     const body = new URLSearchParams();
-    body.set('action', "getFavoritBerita"); 
-    body.set('idUser', idUser.toString()); 
+    body.set('action', 'getFavoritBerita');
+    body.set('idUser', idUser.toString());
     const urlEncodedData = body.toString();
 
     return this.http.post(this.url, urlEncodedData, { headers });
   }
 
-  updateRatingBerita(idBerita:number):Observable<any>{
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+  updateRatingBerita(idBerita: number): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
     const body = new URLSearchParams();
-    body.set('action', "updateRatingBerita"); 
-    body.set('idBerita', idBerita.toString()); 
+    body.set('action', 'updateRatingBerita');
+    body.set('idBerita', idBerita.toString());
     const urlEncodedData = body.toString();
 
     return this.http.post(this.url, urlEncodedData, { headers });
   }
 
-  addViewBerita(idBerita:number):Observable<any>{
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+  addViewBerita(idBerita: number): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
     const body = new URLSearchParams();
-    body.set('action', "addViewBerita");
-    body.set('idBerita', idBerita.toString()); 
+    body.set('action', 'addViewBerita');
+    body.set('idBerita', idBerita.toString());
     const urlEncodedData = body.toString();
 
     return this.http.post(this.url, urlEncodedData, { headers });
   }
 
+  getAllKategory(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+    const body = new URLSearchParams();
+    body.set('action', 'getAllKategory');
+    const urlEncodedData = body.toString();
+    return this.http.post(this.url, urlEncodedData, { headers });
+  }
 
+  getBeritaByKategori(id: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+    const body = new URLSearchParams();
+    body.set('action', 'getBeritaByKategori');
+    body.set('id_kategori', id);
 
-
-
+    return this.http.post(this.url, body.toString(), { headers });
+  }
 }
